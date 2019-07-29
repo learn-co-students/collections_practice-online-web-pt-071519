d1237a2170bf0fbeef4a93b05044dbc2a507edf4
jose luis <cortezluisjose@gmail.com>
2019-07-29 03:37:25 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,33 @@
+def  sort_array_asc(array)
+  
+  
+  
+  
+end
+def sort_array_desc
+  
+  
+  
+  
+end
+def sort_array_char_count
+  
+end
+def swap_elements
+  
+end
+def reverse_array
+  
+end
+def kesha_maker
+  
+end
+deffind_a
 
+end
+def sum_array
+  
+end
+def add_s
+  
+end
